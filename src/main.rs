de4b207b813d3251ce023546452d64dfaf208af9
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2024-11-09 21:49:52 -0800

    Add new tokens

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use core::fmt;
 use std::{fmt::Debug, fs, str::Chars};
 
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
 #[derive(Debug, PartialEq)]
 enum Token {
     Int(u32),
@@ -8,7 +12,14 @@ enum Token {
     AddOp(char),
     MultOp(char),
     Semicolon,
+    LeftParen,
+    RightParen,
+    Assign,
+    Print,
+    Repeat,
     Ident(String),
+    Invalid(char),
+    Default,
 }
 
 impl fmt::Display for Token {
@@ -18,20 +29,27 @@ impl fmt::Display for Token {
 }
 
 fn main() {
+    pretty_env_logger::init();
     read_file("testfiles/input.txt");
 }
 
 fn read_file(path: &str) {
-    // TODO
-    let tokens = lex_string("1234".to_string());
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+    let tokens = lex_string(contents);
     print_tokens(tokens);
 }
 
 fn lex_string(mut remaining_string: String) -> Vec<Token> {
     let mut tokens = vec![];
+    // TODO: Refactor without the possibility of an infinite loop
     loop {
         let result = get_next_token(remaining_string);
         remaining_string = if let Some((token, string)) = result {
+            if token == Token::Ident("".to_string()) {
+                error!("Infinite loop. Remaining: {}", string);
+                break;
+            }
+            debug!("Adding token: {}", token);
             tokens.push(token);
             string
         } else {
@@ -49,8 +67,12 @@ fn get_next_token(remaining_string: String) -> Option<(Token, String)> {
             '+' | '-' => (Token::AddOp(first_char), last_chars.collect::<String>()),
             '*' | '/' | '%' => (Token::MultOp(first_char), last_chars.collect::<String>()),
             ';' => (Token::Semicolon, last_chars.collect::<String>()),
+            '=' => (Token::Assign, last_chars.collect::<String>()),
+            '(' => (Token::LeftParen, last_chars.collect::<String>()),
+            ')' => (Token::RightParen, last_chars.collect::<String>()),
             '0'..='9' => get_number(remaining_chars),
-            _ => get_ident(remaining_chars),
+            'a'..='z' | 'A'..='Z' | '_' | '$' => get_ident(remaining_chars),
+            _ => (Token::Invalid(first_char), last_chars.collect::<String>()),
         })
     } else {
         None
@@ -58,19 +80,39 @@ fn get_next_token(remaining_string: String) -> Option<(Token, String)> {
 }
 
 fn get_ident(ident_and_remaining: Chars) -> (Token, String) {
+    // TODO: This needs major refactoring
+    let mut identifier = String::new();
+    let mut result = (
+        Token::Default,
+        ident_and_remaining.clone().collect::<String>(),
+    );
     for (identifier_end, character) in ident_and_remaining.clone().enumerate() {
         match character {
+            'a'..='z' | 'A'..='Z' | '_' | '$' | _ if identifier_end == result.1.len() - 1 => {
+                result = (
+                    Token::Ident(ident_and_remaining.collect::<String>()),
+                    String::new(),
+                );
+                break;
+            }
             'a'..='z' | 'A'..='Z' | '_' | '$' => (),
             _ => {
                 let mut ident_and_remaining = ident_and_remaining.collect::<String>();
                 let remaining = ident_and_remaining.split_off(identifier_end);
-                return (Token::Ident(ident_and_remaining), remaining);
+                identifier = ident_and_remaining;
+                result = (Token::Ident(identifier.clone()), remaining);
+                break;
             }
         };
     }
+    // Confim identifier is not a reserved keyword
     (
-        Token::Ident(ident_and_remaining.collect::<String>()),
-        String::new(),
+        match identifier.as_str() {
+            "print" => Token::Print,
+            "repeat" => Token::Repeat,
+            _ => result.0,
+        },
+        result.1,
     )
 }
 
@@ -85,7 +127,10 @@ fn get_number(number_and_remaining: Chars) -> (Token, String) {
                 if number_type == Token::Int(0) {
                     number_type = Token::Float(0.0);
                 } else {
-                    panic!("Valid numbers should have no more than one decimal point")
+                    error!(
+                        "Valid numbers should have no more than one decimal point, token invalid"
+                    );
+                    return (Token::Invalid('.'), remaining);
                 }
             }
             '0'..='9' => (),
@@ -135,15 +180,15 @@ mod tests {
 
     #[test]
     fn it_lexes_idents() {
-        let (result, _) = get_ident("test$_ABC".to_string().chars());
-        assert_eq!(result, Ident("test$_ABC".to_string()));
+        let result = get_ident("test$_ABC".to_string().chars());
+        assert_eq!(result, (Ident("test$_ABC".to_string()), String::new()));
     }
 
     #[test]
     fn it_lexes_idents_with_whitespace() {
-        let result = get_ident("test \n nextword".to_string().chars());
+        let response = get_ident("test \n nextword".to_string().chars());
         assert_eq!(
-            result,
+            response,
             (Ident("test".to_string()), " \n nextword".to_string())
         )
     }
@@ -165,4 +210,21 @@ mod tests {
         let (result, _) = get_next_token("   \n\n  test  \n   nextword".to_string()).unwrap();
         assert_eq!(result, Ident("test".to_string()));
     }
+
+    #[test]
+    fn it_lexes_test_line() {
+        let response = lex_string("firstvar = 123".to_string());
+        assert_eq!(
+            response,
+            vec![Ident("firstvar".to_string()), Assign, Int(123)]
+        )
+    }
+
+    #[test]
+    fn it_skips_invalid_chars() {
+        let (result, _) = get_next_token(".523.3".to_string()).unwrap();
+        assert_eq!(result, Invalid('.'))
+    }
+
+    // TODO: Grammar allows for identifiers with numbers at the end
 }
