b5eaf63c3cdfb3eff9a9e9e3b22c301fa4d65022
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2024-12-29 20:06:33 -0800

    Parse nary and composite functions with peekable

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use std::slice::Iter;
+use std::{iter::Peekable, slice::Iter};
 
 use crate::lexer::{AstNumber, Token};
 
@@ -16,11 +16,13 @@ pub struct AstFunction {
     operands: Vec<AstNode>,
 }
 
-pub fn parse_tokens<'a>(tokens: Iter<'a, Token<'a>>) -> Result<AstNode, &'static str> {
+pub fn parse_tokens<'a>(
+    tokens: &mut Peekable<Iter<'a, Token<'a>>>,
+) -> Result<AstNode, &'static str> {
     parse_f_expr(tokens)
 }
 
-fn parse_f_expr(mut tokens: Iter<Token>) -> Result<AstNode, &'static str> {
+fn parse_f_expr<'a>(tokens: &mut Peekable<Iter<'a, Token<'a>>>) -> Result<AstNode, &'static str> {
     if let Some(Token::LeftParen) = tokens.next() {
         if let Some(Token::Func(func_token)) = tokens.next() {
             let operands = match func_token {
@@ -40,8 +42,8 @@ fn parse_f_expr(mut tokens: Iter<Token>) -> Result<AstNode, &'static str> {
     }
 }
 
-fn parse_one_arg(mut tokens: Iter<Token>) -> Result<AstNode, &'static str> {
-    let s_expr = parse_s_expr(&mut tokens);
+fn parse_one_arg<'a>(tokens: &mut Peekable<Iter<'a, Token<'a>>>) -> Result<AstNode, &'static str> {
+    let s_expr = parse_s_expr(tokens);
     if let Ok(s_expr_ok) = s_expr {
         if let Some(Token::RightParen) = tokens.next() {
             Ok(s_expr_ok)
@@ -53,7 +55,9 @@ fn parse_one_arg(mut tokens: Iter<Token>) -> Result<AstNode, &'static str> {
     }
 }
 
-fn parse_two_args<'a>(mut tokens: Iter<'a, Token<'a>>) -> Result<Vec<AstNode>, &'static str> {
+fn parse_two_args<'a>(
+    mut tokens: &mut Peekable<Iter<'a, Token<'a>>>,
+) -> Result<Vec<AstNode>, &'static str> {
     match parse_s_expr(&mut tokens) {
         Ok(first_expr) => match parse_s_expr(&mut tokens) {
             Ok(second_expr) => match tokens.next() {
@@ -66,17 +70,29 @@ fn parse_two_args<'a>(mut tokens: Iter<'a, Token<'a>>) -> Result<Vec<AstNode>, &
     }
 }
 
-fn parse_many_args<'a>(tokens: Iter<'a, Token<'a>>) -> Result<Vec<AstNode>, &'static str> {
-    Err("Not implemented")
+fn parse_many_args<'a>(
+    tokens: &mut Peekable<Iter<'a, Token<'a>>>,
+) -> Result<Vec<AstNode>, &'static str> {
+    let mut operands = Vec::new();
+    while tokens.peek() != Some(&&Token::RightParen) {
+        match parse_s_expr(tokens) {
+            Ok(s_expr) => operands.push(s_expr),
+            Err(msg) => return Err(msg),
+        }
+    }
+
+    return Ok(operands);
 }
 
-fn parse_s_expr(tokens: &mut Iter<Token>) -> Result<AstNode, &'static str> {
+fn parse_s_expr<'a>(tokens: &mut Peekable<Iter<'a, Token<'a>>>) -> Result<AstNode, &'static str> {
     // TODO: Avoid clone
-    // TODO: Other types of nodes, not just a number
-    if let Some(Token::Number(number)) = tokens.next() {
-        Ok(AstNode::NumNode(number.clone()))
-    } else {
-        Err("Composite functions are not yet implemented")
+    match tokens.peek() {
+        Some(Token::Number(number)) => {
+            tokens.next();
+            Ok(AstNode::NumNode(number.clone()))
+        }
+        Some(Token::LeftParen) => parse_f_expr(tokens),
+        _ => Err("Unexpected token for s_expr"),
     }
 }
 
@@ -89,13 +105,13 @@ mod tests {
     #[test]
     fn it_parses_unary_functions() {
         let tokens = vec![LeftParen, Func(Unary(Neg)), Number(Int(3)), RightParen];
-        let tokens = tokens.iter();
+        let mut tokens = tokens.iter().peekable();
         let tree_result = FuncNode(AstFunction {
             func: Unary(Neg),
             operands: vec![NumNode(Int(3))],
         });
         assert_eq!(
-            parse_tokens(tokens).expect("it should parse without throwing an error"),
+            parse_tokens(&mut tokens).expect("It should parse without throwing an error."),
             tree_result
         );
     }
@@ -109,15 +125,74 @@ mod tests {
             Number(Int(5)),
             RightParen,
         ];
-        let tokens = tokens.iter();
+        let mut tokens = tokens.iter().peekable();
         let tree_result = FuncNode(AstFunction {
             func: Binary(Div),
             operands: vec![NumNode(Double(3.14)), NumNode(Int(5))],
         });
 
         assert_eq!(
-            parse_tokens(tokens).expect("it should parse without throwing an error"),
+            parse_tokens(&mut tokens).expect("It should parse without throwing an error."),
             tree_result
         );
+    }
+
+    #[test]
+    fn it_parses_nary_functions() {
+        let tokens = vec![
+            LeftParen,
+            Func(NAry(Add)),
+            Number(Int(111)),
+            Number(Int(222)),
+            Number(Int(333)),
+            Number(Int(444)),
+            RightParen,
+        ];
+        let mut tokens = tokens.iter().peekable();
+        let tree_result = FuncNode(AstFunction {
+            func: NAry(Add),
+            operands: vec![
+                NumNode(Int(111)),
+                NumNode(Int(222)),
+                NumNode(Int(333)),
+                NumNode(Int(444)),
+            ],
+        });
+
+        assert_eq!(
+            parse_tokens(&mut tokens).expect("It should parse without throwing an error."),
+            tree_result
+        )
+    }
+
+    #[test]
+    fn it_parses_composite_functions() {
+        let tokens = vec![
+            LeftParen,
+            Func(NAry(Add)),
+            LeftParen,
+            Func(Binary(Sub)),
+            Number(Int(111)),
+            Number(Int(222)),
+            RightParen,
+            Number(Int(333)),
+            RightParen,
+        ];
+        let mut tokens = tokens.iter().peekable();
+        let tree_result = FuncNode(AstFunction {
+            func: NAry(Add),
+            operands: vec![
+                FuncNode(AstFunction {
+                    func: Binary(Sub),
+                    operands: vec![NumNode(Int(111)), NumNode(Int(222))],
+                }),
+                NumNode(Int(333)),
+            ],
+        });
+
+        assert_eq!(
+            parse_tokens(&mut tokens).expect("It should parse without throwing an error."),
+            tree_result
+        )
     }
 }
